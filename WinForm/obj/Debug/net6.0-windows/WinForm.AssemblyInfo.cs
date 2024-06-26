3cf5f06b96805658fa02d27856a33dac2975d567
Agusslo <agus.lopez042004@gmail.com>
2024-06-25 22:43:11 -0300

    fina

--- WinForm/obj/Debug/net6.0-windows/WinForm.AssemblyInfo.cs
+++ WinForm/obj/Debug/net6.0-windows/WinForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WinForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e68b7764a7cc4ac4a065b3d7a93c9b8108067e3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac419fe241f5088a33a92a94dd77249f12a66cc6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WinForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WinForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
